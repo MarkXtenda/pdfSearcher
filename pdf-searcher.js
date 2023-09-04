e669590abb7591d419e87f0df76c08c0692ff1c3
MarkXtenda <mark.i2p.1998@gmail.com>
2023-09-04 17:28:37 +1000

    3. Function refactoring and simplification.

--- pdf-searcher.js
+++ pdf-searcher.js
@@ -1,4 +1,4 @@
-const fileSystem = require('fs');
+const fileSystem = require('fs').promises;
 const path = require('path');
 const pdf = require('pdf-parse');
 
@@ -6,23 +6,41 @@ const pathToPdf = process.argv[2];
 let pdfFiles = [];
 const keyWord = "Software";
 
-fileSystem.readdir(pathToPdf, (err, files) => {
-    if (err) {
-        return console.log("Reading Dirrectory Error: ", err);
+async function readPdf(data) {
+    try {
+        const pdfResponse =  await pdf(data);
+        return pdfResponse.text;
+    } catch (error) {
+        console.log("PDF error: ", error);
     }
-    pdfPath = path.join(pathToPdf, files[0])
-        fileSystem.readFile(pdfPath, async (err, data)=>{
-            if (err) {
-                return console.log("Reading File Error: ", err);
-            }
-            pdf(data).then((pdfData) => {
-                const text = pdfData.text;
-                console.log(`Text from ${files[0]}:`);
-                console.log(text);
-                const keyWordExist = text.includes(keyWord);
-                console.log(`This text does ${keyWordExist ? "includes" : "not includes"} a keyword ${keyWord}`)
-            }).catch((err) => {
-                console.log("PDF error: ", err);
-            });
-        })
-})
+}
+
+async function readDir(pathToDir) {
+    try {
+        const response = await fileSystem.readdir(pathToDir);
+        return response;
+    } catch (error) {
+        console.log("Reading Dirrectory Error: ", error);
+    }
+}
+
+async function readFiles(pathToPdf, file) {
+    const pdfPath = path.join(pathToPdf, file)
+    try {
+        const fileBuffer = fileSystem.readFile(pdfPath);
+        return fileBuffer;
+    } catch (error) {
+        console.log("Reading File Error: ", err);
+    }
+}
+
+async function pdfSearcher() {
+    const file = await readDir(pathToPdf)
+    const buffer = await readFiles(pathToPdf, file[0])
+    const text = await readPdf(buffer);
+    console.log(`Text from ${file[0]}:`);
+    console.log(text);
+    const keyWordExist = text.includes(keyWord);
+    console.log(`This text does ${keyWordExist ? "includes" : "not includes"} a keyword ${keyWord}`)
+}
+pdfSearcher();
